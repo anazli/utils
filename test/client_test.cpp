9a42f6275ca48fece216b6da42249236e60af021
Andreas Nazlidis <andreas221b@gmail.com>
2026-03-09 19:57:01 +0100

    add client test

---
+++ test/client_test.cpp
@@ -0,0 +1,25 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+
+#include "networking/tcp_client.h"
+
+using namespace testing;
+
+class TcpClientTest : public Test {
+ public:
+  static constexpr auto remote_ip = std::string_view("www.example.com");
+  static constexpr auto local_ip = std::string_view("127.0.0.1");
+  static constexpr auto test_port = std::string_view("8080");
+};
+
+TEST_F(TcpClientTest, GivenValidInputWhenCreatedThenItIsValid) {
+  ASSERT_NO_THROW(net::TcpClient c(remote_ip.data(), test_port.data()));
+}
+
+TEST_F(TcpClientTest,
+       GivenValidSocketWhenItConnectsToAnAddressThenItDoesNoThrow) {
+  net::TcpClient s(remote_ip.data(), test_port.data());
+  ASSERT_NO_THROW(s.connect());
+}
