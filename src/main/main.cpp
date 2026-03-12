4176627f565b3bf067e7a07c9c6a89b70287f63d
Andreas Nazlidis <andreas221b@gmail.com>
2026-03-12 08:57:36 +0100

    add playground target

---
+++ src/main/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "networking/tcp_server.h"
+
+// Playgroud
+
+int main() {
+  net::TcpServer server("localhost", "8080");
+  server.bind();
+  std::cout << "Server is listening..." << std::endl;
+  server.listen(1);
+  std::cout << "Done!" << std::endl;
+  std::cout << "Accepting new connection" << std::endl;
+  auto client = server.accept();
+  std::cout << "On file descriptor:" << client.getHandle() << std::endl;
+
+  return 0;
+}
